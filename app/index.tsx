2ca69bd80248c44551683e044cdca6987908527e
syfhhhh <105841106122@student.unismuh.ac.id>
2025-07-26 23:33:35 +0800

    tgs4

--- app/index.tsx
+++ app/index.tsx
@@ -6,43 +6,57 @@ export default function Index() {
   return (
     <ScrollView contentContainerStyle={{ padding: 20 }}>
 
+      {/* 5 SEBELUM STAMBUK 105841106122 */}
+
+      {/* 1 - Static Font */}
       <Text style={{ fontFamily: "CabinSketch", fontSize: 26, color: "blue" }}>
         {"\n"}==105841105722== (Zaskia aulia ashar)
       </Text>
 
+      {/* 2 - Static Font */}
       <Text style={{ fontFamily: "CormorantUnicase", fontSize: 24, color: "blue" }}>
-        {"\n"}==105841105822== ( MUH. ILHAM AKBAR)
+        {"\n"}==105841105822== (MUH. ILHAM AKBAR)
       </Text>
 
+      {/* 3 - Variable Font */}
       <Text style={{ fontFamily: "CroissantOne", fontSize: 24, color: "blue" }}>
         {"\n"}==105841105922== (ZELVIA)
       </Text>
 
+      {/* 4 - Variable Font */}
       <Text style={{ fontFamily: "DancingScript", fontSize: 24, color: "blue" }}>
         {"\n"}==105841106022== (andi difta rameyza)
       </Text>
 
+      {/* 5 - Static Font */}
       <Text style={{ fontFamily: "Gabriela", fontSize: 24, color: "red", fontWeight: "bold" }}>
         {"\n"}====105841106122==== Arsifah ainun aulia
       </Text>
 
+      {/* 5 SESUDAH STAMBUK 105841106122 */}
+
+      {/* 6 - Variable Font */}
       <Text style={{ fontFamily: "IntelOneMono", fontSize: 24, color: "blue" }}>
         {"\n"}===tegar surya prayoga=== (105841106222)
       </Text>
 
+      {/* 7 - Static Font */}
       <Text style={{ fontFamily: "Niconne", fontSize: 24, color: "blue" }}>
         {"\n"}===andi angke riona megawan=== (105841106322)
       </Text>
 
-      <Text style={{ fontFamily: "DancingScript", fontSize: 24, color: "blue" }}>
+      {/* 8 - Variable Font */}
+      <Text style={{ fontFamily: "Caveat", fontSize: 24, color: "blue" }}>
         {"\n"}===muh. fadli=== (105841106422)
       </Text>
 
-      <Text style={{ fontFamily: "Caveat", fontSize: 24, color: "blue" }}>
+      {/* 9 - Variable Font */}
+      <Text style={{ fontFamily: "PlaywritePL", fontSize: 24, color: "blue" }}>
         {"\n"}===a.fahcri=== (105841106522) 
       </Text>
 
-      <Text style={{ fontFamily: "PlaywritePL", fontSize: 24, color: "blue" }}>
+      {/* 10 - Static Font */}
+      <Text style={{ fontFamily: "DancingScript", fontSize: 24, color: "blue" }}>
         {"\n"}===m. agil gimnas== (105841106622)
       </Text>
 
